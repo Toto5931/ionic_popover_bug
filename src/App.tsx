b7b6678051795a4e2fb719cc8142d7b9bcd8c4a1
Toto5931 <antoine.abele@gmail.com>
2020-05-04 20:58:59 +0200

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
-import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
+import React, { useState } from 'react';
 import {
   IonApp,
   IonIcon,
-  IonLabel,
   IonRouterOutlet,
   IonTabBar,
   IonTabButton,
-  IonTabs
+  IonTabs,
+  IonPopover
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
+import { mailOutline, ellipsisVerticalOutline } from 'ionicons/icons';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -34,33 +30,45 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route path="/tab1" component={Tab1} exact={true} />
-          <Route path="/tab2" component={Tab2} exact={true} />
-          <Route path="/tab3" component={Tab3} />
-          <Route path="/" render={() => <Redirect to="/tab1" />} exact={true} />
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
-  </IonApp>
-);
+const App: React.FC = () => {
+  const [showPopover, setShowPopover] = useState<{ open: boolean, event: Event | undefined }>({
+    open: false,
+    event: undefined,
+  });
+
+  return (
+    <IonApp>
+       <IonReactRouter> 
+        <IonTabs>
+
+          <IonRouterOutlet>
+          </IonRouterOutlet>
+
+          <IonTabBar slot="bottom">
+
+            <IonTabButton tab="tab1" href="/tab1">
+              <IonIcon icon={mailOutline} />
+            </IonTabButton>
+
+            <IonTabButton onClick={(e) => {setShowPopover({ open: true, event: e.nativeEvent });}} >
+              <IonIcon icon={ellipsisVerticalOutline} />
+            </IonTabButton>
+
+          </IonTabBar>
+
+        </IonTabs>
+
+        <IonPopover
+          isOpen={showPopover.open}
+          event={showPopover.event}
+          onDidDismiss={() => setShowPopover({ open: false, event: undefined })}
+        >
+          Text
+        </IonPopover>
+
+      </IonReactRouter>
+    </IonApp>
+  );
+};
 
 export default App;
